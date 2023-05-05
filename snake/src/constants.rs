3661320ce7c9921226dee74bff86551215ea554d
prajnastra <coder.abhijit90@gmail.com>
2023-05-05 12:01:34 +0530

    add: snake

--- snake/src/constants.rs
+++ snake/src/constants.rs
@@ -1,3 +1,30 @@
-use bevy::render::color::Color;
+use bevy::{
+    math::{Vec2, Vec3},
+    render::color::Color,
+};
+
+// Defines the amount of time that should elapse between each physics step.
+pub const TIME_STEP: f32 = 1.0 / 60.0;
+
+// These constants are defined in `Transform` units.
+// Using the default 2D camera they correspond 1:1 with screen pixels.
+pub const SNAKE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
+pub const GAP_BETWEEN_SNAKE_AND_FLOOR: f32 = 60.0;
+pub const SNAKE_SPEED: f32 = 500.0;
+// How close can the paddle get to the wall
+pub const SNAKE_PADDING: f32 = 10.0;
+// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
+pub const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
+pub const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+pub const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+
+pub const WALL_THICKNESS: f32 = 10.0;
+// x coordinates
+pub const LEFT_WALL: f32 = -450.;
+pub const RIGHT_WALL: f32 = 450.;
+// y coordinates
+pub const BOTTOM_WALL: f32 = -300.;
 
 pub const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+pub const SNAKE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+pub const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,8 +1,16 @@
 mod constants;
+mod components;
 
 use bevy::prelude::*;
-use constants::BACKGROUND_COLOR;
-use shared::{WallBundle, WallLocation};
+use constants::{
+    BACKGROUND_COLOR, 
+    GAP_BETWEEN_SNAKE_AND_FLOOR, 
+    BOTTOM_WALL,
+    SNAKE_SIZE,
+    SNAKE_COLOR
+};
+use shared::{WallBundle, WallLocation, Collider};
+use components::Snake;
 
 fn main() {
     App::new()
@@ -15,6 +23,26 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+    
+    // snake
+    let paddle_y = BOTTOM_WALL + GAP_BETWEEN_SNAKE_AND_FLOOR;
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, paddle_y, 0.0),
+                scale: SNAKE_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: SNAKE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Snake,
+        Collider,
+    ));
 
     // Walls
     commands.spawn(WallBundle::new(WallLocation::Left));
