5a5a09aacdf1925880b324d8d9d864dc8be89c62
prajnastra <coder.abhijit90@gmail.com>
2023-05-05 11:51:14 +0530

    add: walls

---
+++ snake/src/constants.rs
@@ -0,0 +1,3 @@
+use bevy::render::color::Color;
+
+pub const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,3 +1,24 @@
+mod constants;
+
+use bevy::prelude::*;
+use constants::BACKGROUND_COLOR;
+use shared::{WallBundle, WallLocation};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_startup_system(setup)
+        .add_system(bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Walls
+    commands.spawn(WallBundle::new(WallLocation::Left));
+    commands.spawn(WallBundle::new(WallLocation::Right));
+    commands.spawn(WallBundle::new(WallLocation::Bottom));
+    commands.spawn(WallBundle::new(WallLocation::Top));
 }
